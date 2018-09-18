7523f06d04f7fcb941d1ee4b3cdd20df11182dcd
Gudni Natan Gunnarsson <gudnig18@ru.is>
2018-09-18 14:47:26 +0000

    tileTraveller

---
+++ tileTraveller2.py
@@ -0,0 +1,88 @@
+def getDirections(x, y):
+    ''' Returns the directions one could travel in from x, y according to the diagram '''
+    n = True
+    s = True
+    e = True
+    w = True
+
+    #check borders
+    if y == 3:
+        n = False
+    elif y == 1:
+        s = False
+    if x == 1:
+        w = False
+    elif x == 3:
+        e = False
+
+
+    #check walls
+
+    if y == 1:
+        e = False
+        w = False
+    elif x == 2 and y == 2:
+        n = False
+        e = False
+    elif x == 3 and y == 2:
+        w = False
+    elif x == 2 and y == 3:
+        s = False
+    
+    return n, e, s, w
+
+def printAvailableDirections(x, y):
+    ''' Prints the directions one could travel in from x, y '''
+    n, e, s, w = getDirections(x, y)
+    print("You can travel:", end=" ")
+    if n:
+        print("(N)orth", end="")
+        if e or w or s:
+            print(" or", end=" ")
+    if e:
+        print("(E)ast", end="")
+        if w or s:
+            print(" or", end=" ")
+    if s:   
+        print("(S)outh", end="")
+        if w:
+            print(" or", end=" ")
+    if w:
+        print("(W)est", end="")
+    print(".")
+
+def getValidInput(x, y):
+    ''' Returns a user inputted direction that one could travel in from x, y '''
+    n, e, s, w = getDirections(x, y)
+    while True:
+        direction = input("Direction: ").upper()
+        if direction == "N" and n:
+            break
+        elif direction == "E" and e:
+            break
+        elif direction == "S" and s:
+            break
+        elif direction == "W" and w:
+            break
+        print("Not a valid direction!")
+    return direction
+
+def movePlayer(x, y, direction):
+    ''' Returns the moved x, y coordinates according to the direction '''
+    if direction == "N":
+        y += 1
+    elif direction == "E":
+        x += 1
+    elif direction == "S":
+        y -= 1
+    elif direction == "W":
+        x -= 1
+    return x, y
+
+
+x, y = 1, 1
+while not (x == 3 and y == 1):
+    printAvailableDirections(x, y)
+    direction = getValidInput(x, y)
+    x, y = movePlayer(x, y, direction)
+print("Victory!")
